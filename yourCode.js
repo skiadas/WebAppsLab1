986053ef33338e7c8b31b37bd71e8dc96f117b14
Luke Lentz <lentzl16@hanover.edu>
2015-01-24 14:18:19 -0500

    isEmpty returns true for new stack

--- yourCode.js
+++ yourCode.js
@@ -4,12 +4,14 @@
  */
 // All your code will go here
 var makeStack = (function() {
+var arr = [];
 var stack = {
 	push: function push(el){
 	},
 	pop: function pop(){
 	},
 	isEmpty: function isEmpty(){
+		return arr.length === 0;	
 	}
 };
 return stack;
