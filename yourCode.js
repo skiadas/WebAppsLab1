ce099b54904a65f5d46d6082d4449879be5b173c
Dakota McCoy <dakotaraymccoy@gmail.com>
2015-01-24 14:44:40 -0600

    issue #6 makestack returns 3 keys

--- yourCode.js
+++ yourCode.js
@@ -1,12 +1,32 @@
 /*
- * Name 1: YourNameHere
- * Name 2: YourNameHere
+ * Name 1: Edwin
+ * Name 2: Dakota
  */
 // All your code will go here
-var makeStack = function() {
 
-	return {};
-};
+
+function makeStack() {
+    var values = [];
+    function push(el) {
+        values.push(el);
+        return stack;
+    };
+    function pop() {
+        if (isEmpty()) {
+            throw new Error("Attempt to pop from empty stack");
+        } else {
+            return values.pop();
+        }
+    };
+    function isEmpty() {
+        return values.length === 0;
+    };
+    return {
+        push: push,
+        pop: pop,
+        isEmpty: isEmpty
+    };
+}
 
 
 // Do NOT change anything below this line.
