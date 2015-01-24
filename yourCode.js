ddb44a54d76ad09584a9a51bbe9989444850b953
Dakota McCoy <dakotaraymccoy@gmail.com>
2015-01-24 15:23:41 -0600

    all tests pass for stack

--- yourCode.js
+++ yourCode.js
@@ -4,28 +4,25 @@
  */
 // All your code will go here
 
-
 function makeStack() {
     var values = [];
-    function push(el) {
-        values.push(el);
-        return stack;
-    };
-    function pop() {
-        if (isEmpty()) {
-            throw new Error("Attempt to pop from empty stack");
-        } else {
-            return values.pop();
+    var stack = {
+        push: function push(el) {
+            values.push(el);
+            return stack;
+        },
+        pop: function pop() {
+            if (stack.isEmpty()) {
+                throw new Error("Attempt to pop from empty stack");
+            } else {
+                return values.pop();
+            }
+        },
+        isEmpty: function isEmpty() {
+            return values.length === 0;
         }
     };
-    function isEmpty() {
-        return values.length === 0;
-    };
-    return {
-        push: push,
-        pop: pop,
-        isEmpty: isEmpty
-    };
+    return stack;
 }
 
 
